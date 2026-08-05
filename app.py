ae02250770547c82b1c6ab6f52248d5fe111db73
devendrabalapurkar98-pixel <devendrabalapurkar98@gmail.com>
2026-08-06 00:37:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ if st.button("Predict Diabeties"):
     prediction = model.predict(input_scaled)
 
     # Display Result
-    if prediction[0] == 0 or prediction[0] == 1:
+    if prediction[0] == 0:
         st.error("The person is Diabetic.")
     else: 
         st.success("The person is not Diabetic.")
